0dedad1293e57b9b59579f5add05d1ca2b2f9593
Prashant Goel (Securities) <PR20145736@wipro.com>
2020-06-17 14:35:09 +0530

    Second commit

---
+++ src/test/java/Maven/Project/Test1.java
@@ -0,0 +1,29 @@
+package Maven.Project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void test1() throws InterruptedException
+	{
+		
+//		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pr20145736\\Downloads\\chromedriver_win32(1)\\chromedriver.exe");
+//		
+//		WebDriver driver=new ChromeDriver();
+//	
+//		driver.get("www.google.com");
+//		
+//		driver.manage().window().maximize();
+//		
+//		Thread.sleep(5000);
+//		
+//		driver.quit();
+//	
+		
+		System.out.println(" Hey this is my text!! ");
+	}
+
+}
